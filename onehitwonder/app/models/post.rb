a2dde5ad264c6ac17e26d171f77533e5da39cd10
justinowusu <justinowusu10@gmail.com>
2018-05-18 14:06:08 -0700

    Update post.rb
    added reddit attributes

--- onehitwonder/app/models/post.rb
+++ onehitwonder/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :feed
-  attr_accessor :title, :url, :urlToImage, :description, :publishedAt
+  attr_accessor :title, :url, :urlToImage, :description, :publishedAt, :author, :score, :subreddit, :numComments,
+                :upvotes, :thumbnail, :commentsLink
   # has_many :TechCrunchPost
   # accepts_nested_attributes_for :TechCrunchPost
 end
