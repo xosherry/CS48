a5ebf7a86933689df3e8da36690fb046a59ac29c
JonathanChen2016 <jonathanchen2016@yahoo.com>
2018-06-01 03:40:30 -0700

    fixed blurry URL, reduced # of yt vids to 20

--- onehitwonder/app/models/youtube_post.rb
+++ onehitwonder/app/models/youtube_post.rb
@@ -8,13 +8,13 @@ class YoutubePost < Post
     end
 
 
-    @youtube_posts = Yt::Collections::Videos.new.where(chart: 'mostPopular')
+    @youtube_posts = Yt::Collections::Videos.new.where(chart: 'mostPopular').take(20)
     list_of_youtube_posts = []
     @youtube_posts.each do |post|
       new_post = Post.new
       new_post.title = post.title
       new_post.url = "https://www.youtube.com/watch?v=" + post.id
-      new_post.urlToImage = post.thumbnail_url
+      new_post.urlToImage = "https://img.youtube.com/vi/" + post.id + "/0.jpg"
       new_post.description = post.description
       new_post.publishedAt = post.published_at.to_s[0,10].split("-").reverse.join("-")
       list_of_youtube_posts << new_post
